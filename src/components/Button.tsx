5eadbd3cd6095e6ecc53cfde821c187994538a4f
YashKrTripathi <yashkr.tripathi@gmail.com>
2026-06-05 14:53:39 +0530

    feat: add chapter print functionality & UI fixes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 
 function Button({
   text,
-  action = '',
+  action,
   color = '',
   textCol = '',
   additional = '',
   hover = '',
 }: {
   text: string;
-  action: any;
-  color: string;
-  textCol: string;
-  additional: string;
-  hover: string;
+  action?: any;
+  color?: string;
+  textCol?: string;
+  additional?: string;
+  hover?: string;
 }) {
   return (
     <button
       className={` ${color ? color : 'bg-primary'} ${textCol ? textCol : ' text-white'} ${additional ? additional : ''} ${hover ? hover : 'hover:bg-primary/60'} uppercase   p-4 pl-8 pr-8   font-bold  transition-all duration-300 cursor-pointer text-[14px]`}
-      onClick={action}
+      onClick={typeof action === 'function' ? action : undefined}
     >
       {text}
     </button>
